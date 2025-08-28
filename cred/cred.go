f0058e307519b6da0642480d13a6eb7087191676
SzymonZet <git@github.com>
2025-08-29 01:20:24 +0200

    Maintenance break support added

--- cred/cred.go
+++ cred/cred.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"syscall"
 	"szymonzet/luxchck/erroring"
+	"time"
 
 	"golang.org/x/term"
 )
@@ -44,32 +45,51 @@ func RefreshHeaderCookie() {
 		Token        string `json:"token"`
 	}{}
 
-	log.Printf("trying to (re)log in...")
-	loginRequestBody, err := json.Marshal(loginReqBody)
-	erroring.QuitIfError(err, "error when trying to marshal auth data")
-
-	payload := strings.NewReader(string(loginRequestBody))
-
-	req, err := http.NewRequest("GET", loginUrl, payload)
-	erroring.QuitIfError(err, "error when trying to create auth request")
-	req.Header.Add("Content-Type", "application/json; charset=utf-8")
-
-	resp, err := http.DefaultClient.Do(req)
-	erroring.QuitIfError(err, "error when getting response from the auth request")
-	defer resp.Body.Close()
-
+	var content []byte
 	var cookieHeaderString strings.Builder
-	for _, cookie := range resp.Cookies() {
-		cookieString := fmt.Sprintf("%v=%v; ", cookie.Name, cookie.Value)
-		_, err := cookieHeaderString.WriteString(cookieString)
-		erroring.QuitIfError(err, "error when trying to build auth header cookie string")
+
+	for {
+		log.Printf("trying to (re)log in...")
+		loginRequestBody, err := json.Marshal(loginReqBody)
+		erroring.QuitIfError(err, "error when trying to marshal auth data")
+
+		payload := strings.NewReader(string(loginRequestBody))
+
+		req, err := http.NewRequest("GET", loginUrl, payload)
+		erroring.QuitIfError(err, "error when trying to create auth request")
+		req.Header.Add("Content-Type", "application/json; charset=utf-8")
+
+		log.Printf("sending login request...")
+		resp, err := http.DefaultClient.Do(req)
+		erroring.QuitIfError(err, "error when getting response from the auth request")
+
+		for _, cookie := range resp.Cookies() {
+			cookieString := fmt.Sprintf("%v=%v; ", cookie.Name, cookie.Value)
+			_, err := cookieHeaderString.WriteString(cookieString)
+			erroring.QuitIfError(err, "error when trying to build auth header cookie string")
+		}
+
+		content, err = io.ReadAll(resp.Body)
+
+		// repeat in case of maintenance
+		if strings.Contains(string(content), "<title>Przerwa techniczna</title>") || strings.Contains(string(content), "<title>Technical maintenance</title>") {
+			log.Println("maintenance break detected, trying again in 5 minutes...")
+			resp.Body.Close()
+			time.Sleep(5 * time.Minute)
+		} else if strings.Contains(string(content), "<html>") {
+			log.Println("some html returned (possibly maintenance), trying again in 5 minutes..")
+			resp.Body.Close()
+			time.Sleep(5 * time.Minute)
+		} else {
+			defer resp.Body.Close()
+			break
+		}
+
+		erroring.QuitIfError(err, "error when getting body from auth request response")
 	}
 
-	content, err := io.ReadAll(resp.Body)
-	erroring.QuitIfError(err, "error when getting body from auth request response")
-
-	err = json.Unmarshal(content, &loginRespBody)
-	erroring.QuitIfError(err, "error when trying to unmarshal auth response")
+	err := json.Unmarshal(content, &loginRespBody)
+	erroring.QuitIfError(err, fmt.Sprintf("error when trying to unmarshal auth response: \n```\n%v\n```\n", string(content)))
 
 	if loginRespBody.ErrorMessage != "" || !loginRespBody.Succeeded {
 		erroring.QuitIfError(fmt.Errorf("login response not as expected, response error message: `%v`", loginRespBody.ErrorMessage), "login error")
